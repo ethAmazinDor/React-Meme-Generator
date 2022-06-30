39a2882c9a601ed7332f60fda14a113724e61a18
Dorian Dubreus <doriandubreus@gmail.com>
2022-06-29 20:58:29 -0400

    Header styled complete

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Header = () => {
+    return (
+        <header className="header-container">
+            <div className="title-container">
+                <h2 className="title">Meme Generator</h2>
+            </div>
+        </header>
+    )
+}
+
+export default Header
