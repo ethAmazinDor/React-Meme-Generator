f1090acab294a177efcefc6716077e48e327c6d4
Dorian Dubreus <doriandubreus@gmail.com>
2022-06-29 20:58:46 -0400

    App.js working properly

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react"
+import Header from "./components/Header"
+import Meme from "./components/Meme"
 
 function App() {
   return (
     <div>
-      hello
+      <Header />
+      <Meme />
     </div>
   );
 }
